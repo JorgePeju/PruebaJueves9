c54a6aa2bc37a2d1ab8115bdd70f63c40c3013ba
JorgePeju <pejuco@gmail.com>
2023-02-09 13:45:26 +0100

    segundo commit

--- Prueba1J9.js
+++ Prueba1J9.js
@@ -12,18 +12,18 @@ function opcion1() {
 
     // usamos condicion if para comparar ambos números, primero para ver si es par y ver si es múltiplo de 25, con doble if
 
-
-    if (n1 != (n1%2 == 1) && n1 == (n1%25==1) && n2 != (n2%2==1) && n2 == (n2%25 == 1)) {
-        alert("Ambos son impares y múltiples de 25") 
+   // atascado en que no soy capaz de poner bien la funcion para que averigue si es par/impar
+    if ((n1%2!=0) && (n1%25==0) && (n2%2!=0) && (n2%25==0)) {
+        alert("Ambos son impares y múltiples de 25");
     } else {
-        if (n1 != n1%2 && n1 == n1%25 && n2 == n2%2 && n2 != n2%25) {
-            alert("El primero es impar y múltiple de 25, pero el segundo no")
+        if ((n1%2!=0) && (n1%25==0) && (n2%2==0) && (n2%25!=0)) {
+            alert("El primero es impar y múltiple de 25, pero el segundo no");
         } else {
-            if (n1 == n1%2 && n1 != n1%25 && n2 != n2%2 && n2 == n2%25) {
-                alert("El segundo es impar y múltiple, pero el primero no")
+            if ((n1%2==0) && (n1%25!=0) && (n2%2!=0) && (n2%25==0)) {
+                alert("El segundo es impar y múltiple, pero el primero no");
             } else {
-                if (n1 == n1%2 && n1 != n1%25 && n2 == n2%2 && n2 != n2%25) {
-                    alert("Ninguno es impar ni múltiplo de 25")
+                if ((n1%2==0) && (n1%25!=0) && (n2%2==0) && (n2%25!=0)) {
+                    alert("Ninguno es impar ni múltiplo de 25");
                 }
             }
         }
